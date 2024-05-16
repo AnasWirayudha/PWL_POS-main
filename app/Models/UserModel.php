f614bdf40af68c6d753c510f322dfaf044c5859f
AnasWirayudha <anaswirayudha01@gmail.com>
2024-05-16 13:58:43 +0700

    Commit

---
+++ app/Models/UserModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class UserModel extends Authenticatable {
+    use HasFactory;
+
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama',
+        'password',
+    ];
+
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        return [];
+    }
+
+    public function level(): BelongsTo {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+}
